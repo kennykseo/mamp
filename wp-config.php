48bf36070074e365ffdf9fa5b31daad6395229ee
kennykseo <kenny.kseo@gmail.com>
2018-03-14 16:45:57 -0500

    added with atom 1

--- wp-config.php
+++ wp-config.php
@@ -39,7 +39,7 @@ define('DB_COLLATE', '');
 
 /**#@+
  * Authentication Unique Keys and Salts.
- *
+ * added with atom 1
  * Change these to different unique phrases!
  * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
  * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
